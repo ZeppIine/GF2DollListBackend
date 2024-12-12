cf40f33119fa4bee42ca97db19110ca464d2474b
ZeppIine <kim0904min@gmail.com>
2024-12-12 14:47:05 +0900

    백엔드 빌드

--- src/main/kotlin/kr/ac/kumoh/ce/s20180147/GF2DollListBackend/controller/GF2DollListController.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/GF2DollListBackend/controller/GF2DollListController.kt
@@ -2,14 +2,14 @@ package kr.ac.kumoh.ce.s20180147.GF2DollListBackend.controller
 
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/message") // 백엔드 작동 확인용
+@CrossOrigin(origins = ["https://gf2dolllist.netlify.app/"])
 class GF2DollListController {
   @GetMapping
-  // TODO: React 주소 변경할 것
-  @CrossOrigin(origins = ["http://localhost:3000"])
+  fun getRoot() = mapOf("status" to "success")
+
+  @GetMapping("/api/message")
   fun getMessage() = mapOf("status" to "success")
 }
